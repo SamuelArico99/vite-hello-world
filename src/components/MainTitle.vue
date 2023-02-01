83fe66898c98837de81c3a8f09211b1a73af87a1
SamuelArico99 <samuel16699.sa@gmail.com>
2023-02-01 16:23:43 +0100

    Fine esercizio

---
+++ src/components/MainTitle.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+
+            message: 'La mia prima App con Vite'
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>
+        {{ message }}
+    </h1>
+</template>
+
+<style scoped>
+button {
+    font-weight: bold;
+}
+</style>
